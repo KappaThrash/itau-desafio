609246a378f856a197db30217c294dc09fd308ed
KappaThrash <danielkuhin@gmail.com>
2026-03-31 01:26:19 -0300

    error handling adicionado

---
+++ src/main/java/com/example/teste/controller/transacao/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.teste.controller.transacao.exceptions;
+
+import com.example.teste.Error.ErrorResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.OffsetDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidDateException.class)
+    public ResponseEntity<?> handleInvalidDate(InvalidDateException ex, HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                OffsetDateTime.now(),
+                422,
+                "Invalid Date",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(errorResponse);
+    }
+}
